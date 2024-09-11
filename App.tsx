37bdd85b888e781e8f1d72b109950ce31ccf3464
Antonio Cardenas <gohan21.d@gmail.com>
2024-09-10 23:14:47 -0700

    Cambios en colores y demas añadidos

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch, Text } from 'react-native';
+import { myColors } from './src/styles/Colors';
+import { ThemeContext } from './src/context/ThemeContext';
+import MyKeyboard from './src/components/MyKeyboard';
+
+
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch
+          value={theme === 'dark'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <h2>Soy un texto pro</h2>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Fso%2Fthug-life-glasses&psig=AOvVaw3QZz2g-yItAozsKHJUaO2d&ust=1726121096983000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPjliqmcuogDFQAAAAAdAAAAABBs" alt="" />
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
---
+++ src/styles/Colors.tsx
@@ -0,0 +1,13 @@
+export const myColors = {
+    light: '#c6e0fa',
+    dark: '#7a0505',
+    blue: '#4B5EFC',
+    btnGray: '#4E505F',
+    btnDark: '#2E2F38',
+    gray: '#747477',
+    black: '#000000',
+    white: '#FFFFFF',
+    result: '#46D5B2',
+    darkred: '#7a0505',
+    violet: '#5e057a',
+};
